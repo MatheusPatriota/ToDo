451f1734583c2710b8a68c345b27faeb5372a05c
MatheusPatriota <mpatriota55@gmail.com>
2021-11-11 10:59:16 -0300

    header background setted

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,16 +1,13 @@
 import "./styles";
-import HeaderStyles from "./styles";
+import { Background, HeaderStyles } from "./styles";
 function Header() {
   return (
     <>
       <HeaderStyles>
-        <div>
-          logo
-        </div>
-        <div>
-          menu
-        </div>
+        <div>logo</div>
+        <div>menu</div>
       </HeaderStyles>
+      <Background />
     </>
   );
 }
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -1,17 +1,22 @@
 import styled from "styled-components";
 const HeaderStyles = styled.header`
-  
   display: flex;
-  justify-content: space-between;
-  
+  justify-content: space-evenly;
+
   width: 100%;
   height: 80px;
   left: 0px;
   top: 0px;
 
-  background: #0D1821;
-  color: #FFFFFF;
-
+  background: #0d1821;
+  color: #ffffff;
 `;
 
-export default HeaderStyles;
+const Background = styled.div`
+  width: 100%;
+  height: 10px;
+
+  background: #fe5f55;
+`;
+
+export { HeaderStyles, Background };
