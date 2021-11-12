c8a7976789c3de1645578a7f407c5b2621001ebd
MatheusPatriota <mpatriota55@gmail.com>
2021-11-11 21:24:24 -0300

    header structure done

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,11 +1,37 @@
 import "./styles";
 import { Background, HeaderStyles } from "./styles";
+import logo from '../../assets/logo.svg'
+
+
 function Header() {
   return (
     <>
       <HeaderStyles>
-        <div>logo</div>
-        <div>menu</div>
+        <div className="logoContainer">
+          <img src={logo} alt="logo ToDo"  className="logo"/>
+        </div>
+        <div className="menuContainer">
+          <ul>
+            <li>
+              <a href="/">
+              início
+
+              </a>
+            </li>
+            <li>
+              <a href="/">
+              nova tarefa
+
+              </a>
+            </li>
+            <li>
+              <a href="/">
+
+              sincronizar celular
+              </a>
+            </li>
+          </ul>
+        </div>
       </HeaderStyles>
       <Background />
     </>
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 const HeaderStyles = styled.header`
   display: flex;
   justify-content: space-evenly;
-
+  
   width: 100%;
   height: 80px;
   left: 0px;
@@ -10,6 +10,44 @@ const HeaderStyles = styled.header`
 
   background: #0d1821;
   color: #ffffff;
+
+  .logoContainer{
+    display: flex;
+    height: 100%;
+    align-items: center;
+    .logo{
+      height: 60%;
+    }
+  }
+
+  .menuContainer{
+    display: flex;
+    height: 100%;
+    justify-content:flex;
+    align-items: center;
+
+    li + li::before {
+    content: " | ";
+}
+
+    ul{
+      display: flex;
+      gap: 12px;
+      text-decoration: none;
+      list-style: none;
+    }
+    ul li a{
+      color: #ffffff;
+      text-transform: uppercase;
+      text-decoration: none;
+    }
+
+    ul li a:hover{
+      color: #FE5F55;
+      text-transform: uppercase;
+      text-decoration: none;
+    }
+  }
 `;
 
 const Background = styled.div`
