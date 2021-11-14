bdb28737045688faa70662c25c44c7478bc4f7a0
MatheusPatriota <mpatriota55@gmail.com>
2021-11-13 22:15:04 -0300

    newTaskPage structure done

--- web/src/views/NewTask/index.tsx
+++ web/src/views/NewTask/index.tsx
@@ -5,7 +5,7 @@ import Icon from "../../components/Icon";
 import NewTaskStyles from "./styles";
 
 function NewTaskPage() {
-  function fazIsso() {
+  function categoriesList() {
     var lista = [];
     for (let index = 1; index <= 9; index++) {
       lista.push(<Icon iconNumber={index} />);
@@ -19,12 +19,44 @@ function NewTaskPage() {
       <NewTaskStyles>
         <Header />
         <body>
-          <div className="categories">{fazIsso()}</div>
-          <div className="title">titulo</div>
-          <div className="description">descricao</div>
-          <div className="date">data</div>
-          <div className="time">hora</div>
-          <div className="finishArea">finish</div>
+          <div className="content">
+            <div className="categories">{categoriesList()}</div>
+            <div className="title">
+              <span>titulo:</span>
+              <input type="text" />
+            </div>
+            <div className="description">
+              <span>Descrição:</span>
+              <textarea
+                name="description"
+                id="description"
+                cols={60}
+                rows={10}
+              ></textarea>
+            </div>
+            <div className="date">
+              <span>Data:</span>
+              <input type="text" />
+            </div>
+            <div className="time">
+              <span>Hora:</span>
+              <input type="text" />
+            </div>
+            <div className="finishArea">
+              <div className="text">
+                <div className="checkboxArea">
+                  <input type="checkbox" />
+                  <span>Concluído</span>
+                </div>
+                <div className="checkboxArea">
+                  <span>Excluir</span>
+                </div>
+              </div>
+              <button>
+                Salvar
+              </button>
+            </div>
+          </div>
         </body>
         <Footer />
       </NewTaskStyles>
--- web/src/views/NewTask/styles.ts
+++ web/src/views/NewTask/styles.ts
@@ -1,18 +1,76 @@
 import styled from "styled-components";
 const NewTaskStyles = styled.div`
-  body{
+  body {
     display: flex;
     flex-direction: column;
 
     align-items: center;
-    margin-top:40px;
+    margin-top: 40px;
 
-    .categories{
+    .categories {
       margin: 10px;
 
       display: flex;
       gap: 20px;
-      
+    }
+
+    .title,
+    .description,
+    .date,
+    .time {
+      margin: 10px;
+      display: flex;
+      flex-direction: column;
+
+      span {
+        text-transform: capitalize;
+        font-weight: 700;
+      }
+    }
+
+    .finishArea {
+      display: flex;
+      flex-direction: column;
+
+      .text {
+        display: flex;
+        flex-direction: row;
+
+        justify-content: space-between;
+        align-items: center;
+
+        padding: 10px;
+
+        .checkboxArea {
+          display: flex;
+          flex-direction: row;
+
+          align-items: center;
+
+          span {
+            margin-left: 5px;
+            text-transform: uppercase;
+          }
+        }
+      }
+
+      button {
+        border-radius: 10px;
+        border: none;
+
+        height: 30px;
+
+        color: #fff;
+        text-transform: uppercase;
+        font-weight: 700;
+        background-color: #fe5f55;
+
+        cursor: pointer;
+      }
+
+      button:hover {
+        opacity: 0.85;
+      }
     }
   }
 `;
