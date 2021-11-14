d63c99dd1df307dd9f9c44769429012abeeb57d8
MatheusPatriota <mpatriota55@gmail.com>
2021-11-13 21:46:41 -0300

    categories added to new-task

--- web/src/views/NewTask/index.tsx
+++ web/src/views/NewTask/index.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import Footer from '../../components/Footer';
-import { Header } from '../../components/Header';
+import React from "react";
+import Footer from "../../components/Footer";
+import { Header } from "../../components/Header";
+import Icon from "../../components/Icon";
+import NewTaskStyles from "./styles";
 
 function NewTaskPage() {
+  function fazIsso() {
+    var lista = [];
+    for (let index = 1; index <= 9; index++) {
+      lista.push(<Icon iconNumber={index} />);
+    }
+    return lista;
+  }
 
   return (
     // isso é um fragment
     <>
-      <Header/>
-      <body>
-        Oi mundo eu sou o corpo, sou exclusivo
-      </body>
-      <Footer/>
+      <NewTaskStyles>
+        <Header />
+        <body>
+          <div className="categories">{fazIsso()}</div>
+          <div className="title">titulo</div>
+          <div className="description">descricao</div>
+          <div className="date">data</div>
+          <div className="time">hora</div>
+          <div className="finishArea">finish</div>
+        </body>
+        <Footer />
+      </NewTaskStyles>
     </>
   );
-
 }
 
 export { NewTaskPage };
--- web/src/views/NewTask/styles.ts
+++ web/src/views/NewTask/styles.ts
@@ -1,7 +1,20 @@
 import styled from "styled-components";
-const Header = styled.header`
-  margin: 0;
-  background-color: red;
+const NewTaskStyles = styled.div`
+  body{
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    margin-top:40px;
+
+    .categories{
+      margin: 10px;
+
+      display: flex;
+      gap: 20px;
+      
+    }
+  }
 `;
 
-export default Header;
+export default NewTaskStyles;
