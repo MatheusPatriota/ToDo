5854bead33455a3712cfae9884a535c76001e0f1
MatheusPatriota <mpatriota55@gmail.com>
2021-11-13 22:22:53 -0300

    newTaskPage styles done

--- web/src/views/NewTask/styles.ts
+++ web/src/views/NewTask/styles.ts
@@ -22,9 +22,38 @@ const NewTaskStyles = styled.div`
       display: flex;
       flex-direction: column;
 
+      input {
+        height: 35px;
+        border-radius: 10px;
+        border: none;
+
+        box-shadow: 1px 1px 4px 4px rgba(0, 0, 0, 0.1);
+      }
+
+      input:focus {
+        outline: none !important;
+        border: 1px solid  #fe5f55;
+        box-shadow: 0 0 10px #719ece;
+      }
+
+      textarea {
+        border-radius: 10px;
+        border: none;
+
+        box-shadow: 1px 1px 4px 4px rgba(0, 0, 0, 0.1);
+      }
+
+      textarea:focus {
+        outline: none !important;
+        border: 1px solid #fe5f55;;
+        box-shadow: 0 0 10px #719ece;
+      }
+
       span {
         text-transform: capitalize;
         font-weight: 700;
+
+        margin-bottom: 10px;
       }
     }
 
